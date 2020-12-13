30cf8a06a08b99a9ced40a43e80dd8a185527623
Simo Virokannas <simo@virokannas.fi>
2020-12-13 12:33:43 -0500

    Initial Commit

---
+++ holopointer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  holopointer
+//
+//  Created by Simo Virokannas on 12/13/20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ holopointer/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  holopointer
+//
+//  Created by Simo Virokannas on 12/13/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
